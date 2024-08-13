cc73faa906c3e010d8741596de7d82982d36d57b
HongYongWoo <bluebox587@gmail.com>
2024-08-13 20:16:08 +0900

    feature/Script Script 클래스 추가 StringBuilder로 구현 항목 수정 몇 추가 필요

--- src/main/java/com/ssg/Main.java
+++ src/main/java/com/ssg/Main.java
@@ -1,5 +1,14 @@
 package main.java.com.ssg;
 
+import main.java.com.ssg.library.Script;
+
 public class Main {
 
+  public static Script  script = new Script();
+
+  public static void main(String[] args) {
+    script.printStartMenu();
+    System.out.println();
+;
+  }
 }
