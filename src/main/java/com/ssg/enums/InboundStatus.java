3171b8064c50b51130f1e0ff57dcaf43e984d814
Park Taehwan <xoghks211@naver.com>
2024-08-13 21:05:24 +0900

    :sparkles: 입고 상태를 나타내는 enum 클래스를 추가하였습니다.

--- src/main/java/com/ssg/enums/InboundStatus.java
+++ src/main/java/com/ssg/enums/InboundStatus.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 @Getter
 public enum InboundStatus {
 
-  FENDING_REQUEST("입고요청 대기"),
-  APPROVED_REQUEST("입고요청 수정"),
-  COMPLETED_REQUEST("입고요청 완료"),
-  REJECTED_REQUEST("입고요청 거절");
+  PENDING_REQUEST("입고요청 대기"),
+  ADJUSTING_REQUEST("입고요청 수정"),
+  REJECTED_REQUEST("입고요청 거절"),
+  APPROVED_REQUEST("입고대기"),
+  COMPLETED_INBOUND("입고완료");
 
   InboundStatus(String value) {
     this.value = value;
